4c5590a919d099af735b7d256d462388dab68b7f
vishwajeeta <vishwajeetvishwa4@gmail.com>
2023-07-20 06:51:39 +0530

    modifier for function

---
+++ blockchain/metacrafters/modifiers.sol
@@ -0,0 +1,20 @@
+/**
+modifier is a bit of code that determine if a function is applyes to will execute or not
+Note:- best practice to all the modifier to declare for all the functions we declare.
+ */
+pragma solidity 0.8.0;
+contract greate {
+    uint a;
+    address owner;
+
+constructor ( )  {
+    owner=msg.sender;
+}
+modifier onlyOwner{
+    require(owner==msg.sender,"only owner can access");
+    _;
+}
+function set(uint _a)public onlyOwner{
+    a=_a;
+}
+}
