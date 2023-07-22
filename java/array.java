31b5b9163d90a941c148fe2abe064a93169eb2ee
vishwajeeta <vishwajeetvishwa4@gmail.com>
2023-07-22 09:31:13 +0530

    array and arr.length

---
+++ java/array.java
@@ -0,0 +1,33 @@
+class Vishwa
+{
+    public static void main(String []args)
+    {
+        int array[]={1,3,4,7,6,8,9};
+        
+        
+        System.out.println(array[0]);
+        System.out.println("hello wrold");
+        for(int i=0;i<array.length;i++)
+        System.out.println(array[i]);
+        System.out.println("hii\n");
+        
+
+        Vishwa data=new Vishwa();
+        data.golu();
+        omg();
+
+    }
+    void golu()
+    {
+        
+        String arr[]=new String[10];
+        arr[5]="vishwa";
+        arr[8]="vishwa";
+        for(int i=0;i<arr.length;i++)
+        System.out.println(arr[i]);
+    }
+    public static void omg()
+    {
+        System.out.println("array operation is performed");
+    }
+}
