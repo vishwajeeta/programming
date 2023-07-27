f8e27491b78ebe36306813bdd9dc0855e6510d7c
vishwajeeta <vishwajeetvishwa4@gmail.com>
2023-07-27 20:35:25 +0530

    provides info about blocks in the blockchain

---
+++ blockchain/metacrafters/global functions/block_context.sol
@@ -0,0 +1,9 @@
+/**
+build-in functions exist in global name space
+which provides info about blocks in the blockchain
+1.block.coinbase //address of miner that mines the current block
+2.block.difficulty //it returns the difficulty at the time when the current block was mined
+3.block.timestamp //timestamp current block was mined
+4.block.gaslimit //total gas limit of all the transactions mind on the current block
+5.block.number //number of the newest block in the blockchain
+ */
