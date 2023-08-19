ebb514ca2144cb5021db1e354a07f287fa2e27a2
vishwajeeta <vishwajeetvishwa4@gmail.com>
2023-08-19 07:14:33 +0530

    updated

--- blockchain/solidity chitseet/constructor.sol
+++
@@ -1,8 +0,0 @@
-
-//constructor function
-//constructor that get called when ever we put contract on the blockchain
-//it runs only once,initialise the smart contract with certain value
-constructor(string memory _name,uint _initialCount){
-        name=_name;
-        count=_initialCount;
-    }
