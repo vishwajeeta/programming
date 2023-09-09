bd287d6f561a28b7dca90c3ce7ecbecc50c32fa2
vishwajeeta <vishwajeetvishwa4@gmail.com>
2023-09-09 07:37:57 +0530

    primitive data

---
+++ java/codeWithMosh/primitive.java
@@ -0,0 +1,11 @@
+class sample{
+    public static void main(String[]args){
+        byte age=30;
+        long viewCount=3_123_456_789L;
+        float price=10.99F;
+        char letter='A';
+        boolean isEligible=false;
+        System.out.println("hello world");
+        System.out.println(age+"\n"+viewCount+"\n"+price+"\n"+letter+"\n"+isEligible);
+    }
+}
