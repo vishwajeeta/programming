4bbde6526a1dd9a5e90a09d903d153dea34a7019
vishwajeeta <vishwajeetvishwa4@gmail.com>
2023-09-09 07:53:31 +0530

    always need to assign memory

---
+++ java/codeWithMosh/reference.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+class sample{
+    public static void main(String[]args){
+        Date now=new Date();
+        System.out.println("Date and time:"+now);
+        
+    }
+}
