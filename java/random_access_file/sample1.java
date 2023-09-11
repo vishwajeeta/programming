6140c5ab32c0cfad1d9042fdfa8dfade9a654caa
vishwajeeta <vishwajeetvishwa4@gmail.com>
2023-09-11 19:08:44 +0530

    read file in a loop & throw error if find

---
+++ java/random_access_file/sample1.java
@@ -0,0 +1,16 @@
+import java.io.RandomAccessFile;
+import java .io.IOException;
+
+public class Sample1{
+    public static void main(String [] args) throws IOException
+    {
+        RandomAccessFile raf=new RandomAccessFile("index.txt","rw");
+        while(true){
+            String s=raf.readLine();
+            if(s==null){
+                break;
+            }
+            System.out.println(Upper(s));
+        }
+    }
+}
