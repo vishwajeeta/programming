fc3a1d8e11dc92fe0d5fb66d3bf8544a249b7a41
vishwajeeta <vishwajeetvishwa4@gmail.com>
2023-12-26 17:57:52 +0530

    Swapping in python

---
+++ python/swaping.py
@@ -0,0 +1,5 @@
+a=int(input("Enter first value:"))
+b=int(input("Enter second value:"))
+print(f"Before Swapping:-\nFirst={a}\nSecond={b}")
+a,b=b,a
+print(f"After Swapping:-\nFirst={a}\nSecond={b}")
