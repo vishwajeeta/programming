7d9df4229434c2b61998318aee465c33c24c8077
vishwajeeta <gs699542@gmail.com>
2024-07-10 14:20:02 +0530

    updated

---
+++ python/sample/main.py
@@ -0,0 +1,11 @@
+print("hello world")
+lis1=[]
+lis1.append("hii")
+lis1.append("hii")
+lis1+=lis1
+lis2=[1,1,2,3,4]
+#count the number of dublicate elements in the list
+print(lis2.count(1))
+
+lis1.append(lis2)
+print(lis1)
